1e805ddbb1d74fc99db47fafe30ac617ec6827e3
robinlinus <robinlinus@users.noreply.github.com>
2017-12-06 20:39:02 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # nano-api
-A high-level API to the Nimiq nano client
+A high-level API to the Nimiq nano client. This is intended to be the basis for nimiq wallet applications. 
+
+### Setup
+ - Import nimiq nano client: `<script src="https://cdn.nimiq.com/core/nimiq.js"></script>`
+ - Import high-level API: `<script src="nano-api.js"></script>`
+ - Create an Instance `const nimiq = new NimiqNano()`
+
+### My Address
+`nimiq.address` 
+
+### My Balance
+`nimiq.balance` 
+
+### Send Funds
+- API `nimiq.sendTransaction(recipient, value, fee)` 
+- Example: `nimiq.sendTransaction('NQ50 XXYT 3JT3 LGMQ B3QQ VH7E HXPY 534Q JVR8', 100000000, 1000)` 
+
+### Events
+- `nimiq.onConsensusEstablished()`
+- `nimiq.onBalanceChanged(balance)`
+- `nimiq.onTransactionReceived(sender, value, fee)`
---
+++ nano-api.js
@@ -0,0 +1,76 @@
+class NimiqNano {
+    constructor() {
+        Nimiq.init(() => this.init(), console.error);
+    }
+
+    async init() {
+        const $ = {};
+        $.consensus = await Nimiq.Consensus.nano();
+        $.wallet = await Nimiq.Wallet.getPersistent();
+        $.consensus.on('established', e => this._onConsensusEstablished());
+        $.consensus.network.connect();
+        $.consensus.blockchain.on('head-changed', e => this._headChanged());
+        $.consensus.mempool.on('transaction-added', tx => this._transactionAdded(tx));
+        this.$ = $;
+    }
+
+    async _headChanged() {
+        if (!this.$.consensus.established) return;
+        const balance = await this._getBalance();
+        if (this._balance === balance) return;
+        this._balance = balance;
+        this.onBalanceChanged(balance);
+    }
+
+    async _getAccount() {
+        const account = await this.$.consensus.getAccount(this.$.wallet.address);
+        return account.balance;
+    }
+
+    async _getBalance() {
+        const account = await this._getAccount();
+        return account.value;
+    }
+
+    _onConsensusEstablished() {
+        this._headChanged();
+        this.onConsensusEstablished();
+    }
+
+    async _transactionAdded(tx) {
+        if (!tx.recipientAddr.equals(this.$.wallet.address)) return;
+        const senderAddr = await tx.senderPubKey.toAddress();
+        this.onTransactionReceived(senderAddr.toUserFriendlyAddress(), tx.value, tx.fee);
+    }
+
+    /*
+        Public API
+    */
+    async sendTransaction(recipient, value, fee) {
+        const recipientAddr = Nimiq.Address.fromUserFriendlyAddress(recipient);
+        const account = await this._getAccount();
+        const tx = await this.$.wallet.createTransaction(recipientAddr, value, fee, account.nonce);
+        return this.$.consensus.relayTransaction(tx);
+    }
+
+    get address() {
+        return this.$.wallet.address.toUserFriendlyAddress();
+    }
+
+    get balance() {
+        return this._balance || 0;
+    }
+
+    onConsensusEstablished() {
+        console.log('consensus established');
+    }
+
+    onBalanceChanged(balance) {
+        console.log('new balance:', balance);
+    }
+
+    onTransactionReceived(sender, value, fee) {
+        console.log('received:', value, 'from:', sender, 'txfee:', fee);
+    }
+}
+
