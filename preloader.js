550bf0b7d3df9b31aa8dac5d68fa54f23c1e0e94
robinlinus <robinlinus@users.noreply.github.com>
2017-12-19 14:52:26 -0600

    Add preloader

---
+++ preloader.js
@@ -0,0 +1,14 @@
+class Preloader {
+    static isFirstVisit() {
+        return new Promise(resolve => {
+            const request = indexedDB.open('wallet');
+            request.onupgradeneeded = function(e) {
+                e.target.transaction.abort();
+                resolve(true)
+            }
+            request.onsuccess = function(e) {
+                resolve(false)
+            }
+        })
+    }
+}
