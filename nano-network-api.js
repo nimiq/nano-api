62b6cbb5034bde88e6be340db3d9c6b3f059ea48
sisou <hello@soerenschwert.de>
2018-03-02 18:39:08 +0100

    Return promise from init()

--- nano-network-api.js
+++ nano-network-api.js
@@ -28,6 +28,7 @@ export default class NanoNetworkApi {
         }, e => {
             this.onDifferentTabError(e);
         });
+        return this._apiInitialized;
     }
 
     async _onApiReady() {
