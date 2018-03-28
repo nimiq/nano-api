d69a24ac1a29da1bd5e39f35f7fe0612f1db08fd
danimoh <github.danimoh@neverbox.com>
2018-03-28 11:38:49 -0600

    use validation utils

--- nano-api.js
+++ nano-api.js
@@ -1,3 +1,5 @@
+import ValidationUtils from '/libraries/nimiq-utils/validation-utils/validation-utils.js';
+
 export default class NanoApi {
 
     static get API_URL() { return 'https://cdn.nimiq-network.com/branches/master/nimiq.js' }
@@ -251,41 +253,11 @@ export default class NanoApi {
     }
 
     static validateAddress(address) {
-        try {
-            this.isUserFriendlyAddress(address);
-            return true;
-        } catch (e) {
-            return false;
-        }
+        return ValidationUtils.isValidAddress(address);
     }
 
-    // Copied from: https://github.com/nimiq-network/core/blob/master/src/main/generic/consensus/base/account/Address.js
-
-    static isUserFriendlyAddress(str) {
-        str = str.replace(/ /g, '');
-        if (str.substr(0, 2).toUpperCase() !== 'NQ') {
-            throw new Error('Addresses start with NQ', 201);
-        }
-        if (str.length !== 36) {
-            throw new Error('Addresses are 36 chars (ignoring spaces)', 202);
-        }
-        if (this._ibanCheck(str.substr(4) + str.substr(0, 4)) !== 1) {
-            throw new Error('Address Checksum invalid', 203);
-        }
-    }
-
-    static _ibanCheck(str) {
-        const num = str.split('').map((c) => {
-            const code = c.toUpperCase().charCodeAt(0);
-            return code >= 48 && code <= 57 ? c : (code - 55).toString();
-        }).join('');
-        let tmp = '';
-
-        for (let i = 0; i < Math.ceil(num.length / 6); i++) {
-            tmp = (parseInt(tmp + num.substr(i * 6, 6)) % 97).toString();
-        }
-
-        return parseInt(tmp);
+    static isValidHash(hash) {
+        return ValidationUtils.isValidHash(hash);
     }
 
     static _importApi() {
@@ -297,15 +269,6 @@ export default class NanoApi {
             script.addEventListener('error', () => reject(script), false);
             document.body.appendChild(script);
         });
-    }
-
-    static isValidHash(hash) {
-        // not using Nimiq Api here to don't require it to be loaded already
-        try {
-            return atob(hash).length === 32;
-        } catch(e) {
-            return false;
-        }
     }
 
     setXElement(xElement) {
